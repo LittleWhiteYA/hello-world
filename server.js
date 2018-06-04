3d0b4c114f0fadba6245993725a4cdf0e21ce6ed
Evan Ye <jigsaw.ye@gmail.com>
2018-06-04 18:25:25 +0800

    Update coding style

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var http = require('http');
-var handleRequest = function(request, response) {
+const http = require('http');
+const handleRequest = (request, response) => {
   response.writeHead(200);
   response.end('Hello World!');
 };
-var server = http.createServer(handleRequest);
+const server = http.createServer(handleRequest);
 
 server.listen(8080, () => {
   console.log('server is running on http://localhost:8080');
